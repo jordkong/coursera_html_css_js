bd2765c7470ccfe1b48f749f44b1be2d7c0573fe
unknown <e0540507@u.nus.edu>
2021-06-04 14:21:35 +0800

    JavaScript Added

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -1,1 +1,10 @@
-var speakWord = "Hello";
+(function (window) {
+	var helloSpeaker = {};
+	var speakWord = "Hello";
+	
+	helloSpeaker.speak = function (name) {
+		console.log(speakWord + " " + name)
+	}
+
+	window.helloSpeaker = helloSpeaker;
+})(window);
